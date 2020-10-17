9be839b840a17b67e209f23f2e0b0a54513a29e1
Aparajita Pandey <aparajita.pandey@wheelseye.com>
2020-10-18 02:44:12 +0530

    First Commit

---
+++ BiweeklyContest-37/Mean of Array After Removing Some Elements/Solutions.java
@@ -0,0 +1,26 @@
+/**
+ * Link - https://leetcode.com/contest/biweekly-contest-37/problems/mean-of-array-after-removing-some-elements/
+ * Given an integer array arr, return the mean of the remaining integers after removing the smallest 5% and the largest 5% of the elements.
+ * Answers within 10-5 of the actual answer will be considered accepted.
+ *
+ * Example 1
+ * Input: arr = [6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0]
+ * Output: 4.00000
+ *
+ * Example 2
+ * Input: arr = [6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]
+ * Output: 4.77778
+ *
+ */
+class Solution {
+    public double trimMean(int[] arr) {
+        int len = arr.length;
+        int removed = (len*5)/100;
+        Arrays.sort(arr);
+        int sum = 0;
+        for(int i=removed ; i<len - removed ; i++){
+            sum += arr[i];
+        }
+        return (double)sum/(len - removed*2);
+    }
+}
